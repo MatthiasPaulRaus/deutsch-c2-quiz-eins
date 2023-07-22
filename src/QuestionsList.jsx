23b33ed55b70620cb4ad126e53e465cff0dd0c7a
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-07-22 13:19:08 +0200

    initial commit

---
+++ src/QuestionsList.jsx
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Ich wäre so gern ein guter Koch.",
+        answersList: [
+            {answer:"Hättest du richtig kochen gelernt, wärst du ein guter Koch geworden.", isCorrect:true},
+            {answer:"Weil du richtig kochen kannst, bist du ein guter Koch.", isCorrect:false},
+            {answer:"Obwohl du gut kochen kannst, mag ich dein Essen nicht.", isCorrect:false},
+        ]
+    },{
+        question:"Wenn es mehr regnen würde, müssten wir kein Wasser sparen.",
+        answersList: [
+            {answer:"Wir sollten uns darüber keine Gedanken machen.", isCorrect:false},
+            {answer:"Wir sollten unbedingt sparsam mit dem Wasser umgehen.", isCorrect:true},
+            {answer:"Weil es genug regnet, müssen wir kein Wasser sparen.", isCorrect:false},
+        ]
+    },{
+        question:"Dem Trainer des Clubs wurde gekündigt.",
+        answersList: [
+            {answer:"Trotzdem hätte man ihm kündigen sollen.", isCorrect:false},
+            {answer:"Außerdem hätte er ja auch kündigen können.", isCorrect:false},
+            {answer:"Ihm hätte nicht gekündigt werden dürfen.", isCorrect:true},
+        ]
+    },{
+        question:"Ohne Erhöhung des Eintrittsgeldes",
+        answersList: [
+            {answer:"hätte das Museum schließen müssen.", isCorrect:true},
+            {answer:"ist das Museum in finanziellen Schwierigkeiten.", isCorrect:false},
+            {answer:"wäre das Museum heute nicht geschlossen.", isCorrect:false},
+        ]
+    },{
+        question:"Obwohl der Politiker seine Aussage korrigiert hatte,",
+        answersList: [
+            {answer:"wollte er den Wählern vertrauen.", isCorrect:false},
+            {answer:"wollten die Wähler ihm nicht vertrauen.", isCorrect:true},
+            {answer:"wurde er nicht vertraulich behandelt.", isCorrect:false},
+        ]
+    },{
+        question:"Wenn Sie die Anweisungen genau befolgt hätten,",
+        answersList: [
+            {answer:"hätte wir Ihnen nicht geschadet.", isCorrect:false},
+            {answer:"hätte der Schaden nicht vermieden werden können.", isCorrect:false},
+            {answer:"wäre Ihnen dieser Schaden nicht entstanden.", isCorrect:true},
+        ]
+    },{
+        question:"Heutzutage ist die Situation extrem kompliziert.",
+        answersList: [
+            {answer:"Wenn die Entwicklung anders verlaufen wäre, wäre es nicht so.", isCorrect:true},
+            {answer:"Wenn es sich weiter so entwickelt, wird es bald besser.", isCorrect:false},
+            {answer:"Wenn wir es rückgängig machen könnten, wäre es noch schlechter.", isCorrect:false},
+        ]
+    },{
+        question:"Durch das breite Angebot im Internet",
+        answersList: [
+            {answer:"werden die Abende im Fernsehen umfangreicher.", isCorrect:false},
+            {answer:"werden die Fernsehabende abwechslungsreicher.", isCorrect:true},
+            {answer:"können wir öfter die Abende miteinander verbringen.", isCorrect:false},
+        ]
+    },{
+        question:"Ich konnte gar nicht auf seine Kritik reagieren.",
+        answersList: [
+            {answer:"Deshalb lasse ich da gar nicht mit mir reden.", isCorrect:false},
+            {answer:"Endlich hat man es zur Sprache gebracht.", isCorrect:false},
+            {answer:"Das hat mir völlig die Sprache verschlagen.", isCorrect:true},
+        ]
+    },{
+        question:"Wenn sie spricht, gestikuliert sie sehr stark.",
+        answersList: [
+            {answer:"Ja, sie redet mit Händen und Füßen.", isCorrect:true},
+            {answer:"Ja, sie nimmt sich den Mund zu voll.", isCorrect:false},
+            {answer:"Ja, sie hat gut reden.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Er sagte er sei erschüttert über den tragischen Unfall.",
+        answersList: [
+            {answer:"Der Minister ist von dem Unfall sehr betroffen.", isCorrect:true},
+            {answer:"Der Politiker war vom dem Unfall überrascht.", isCorrect:false},
+            {answer:"Der Journalist hat nichts über den Unfall berichtet.", isCorrect:false},
+        ]
+    },{
+        question:"Wer das getan hat, muss dafür bestraft werden.",
+        answersList: [
+            {answer:"Er sollte deshalb die Verantwortung übernehmen.", isCorrect:false},
+            {answer:"Er solle dafür zur Rechenschaft gezogen werden.", isCorrect:true},
+            {answer:"Er könne sich an die Strafen nicht erinnern.", isCorrect:false},
+        ]
+    },{
+        question:"Als man ihn um eine Stellungnahme bat,",
+        answersList: [
+            {answer:"entgegnete er, das wäre nicht seine Auffassung.", isCorrect:false},
+            {answer:"teilte er mit, dass er das nicht erwarte.", isCorrect:false},
+            {answer:"erwiderte er, das sei nicht sein Verschulden.", isCorrect:true},
+        ]
+    },{
+        question:"Die Subventionen für die Hersteller wurden gestrichen.",
+        answersList: [
+            {answer:"Die bisherigen Empfänger waren darüber empört.", isCorrect:true},
+            {answer:"Die Industrieanlagen sind in einem schlechten Zustand.", isCorrect:false},
+            {answer:"Die Endverbraucher erwarten Preissenkungen.", isCorrect:false},
+        ]
+    },{
+        question:"Ein Team aus Experten untersucht die Unfallursache.",
+        answersList: [
+            {answer:"Die Leute vom Fach unternehmen einen Versuch.", isCorrect:false},
+            {answer:"Die Spezialisten forschen nach dem Grund des Geschehens.", isCorrect:true},
+            {answer:"Die Forscher suchen nach einer Urkunde.", isCorrect:false},
+        ]
+    },{
+        question:"Die Nachfrage ist so stark gesunken, ",
+        answersList: [
+            {answer:"weil die Firma viel Geld verloren hat.", isCorrect:false},
+            {answer:"dass mit höheren Preisen gerechnet werden kann.", isCorrect:false},
+            {answer:"dass die Firma mit hohen Verlusten rechnen muss.", isCorrect:true},
+        ]
+    },{
+        question:"Ich nehme Ihre Kritik zum Anlass,",
+        answersList: [
+            {answer:"um für mehr Verständnis zu werben.", isCorrect:true},
+            {answer:"damit wir die Probleme besser verschweigen.", isCorrect:false},
+            {answer:"um Werbung für unsere Probleme zu machen.", isCorrect:false},
+        ]
+    },{
+        question:"Gegenüber Online-Käufen finden wir viele Objekte auf dem Flohmarkt.",
+        answersList: [
+            {answer:"Wir kaufen zwar online, aber nicht auf dem Flohmarkt.", isCorrect:false},
+            {answer:"Wir kaufen online, finden aber auch vieles auf dem Flohmarkt.", isCorrect:true},
+            {answer:"Wir finden viel auf dem Flohmarkt, dagegen kaufen wir nichts online.", isCorrect:false},
+        ]
+    },{
+        question:"Der Minister erschien nicht zur Pressekonferenz.",
+        answersList: [
+            {answer:"Und ebenso war sein Pressesprecher zu sehen.", isCorrect:false},
+            {answer:"Anstelle des Pressesprechers war niemand gekommen.", isCorrect:false},
+            {answer:"Stattdessen schickte er seinen Pressesprecher.", isCorrect:true},
+        ]
+    },{
+        question:"Zwecks besserem Artenschutz darf man das Gelände nicht mehr befahren.",
+        answersList: [
+            {answer:"Um die hier lebenden Arten zu schützen, hat man das Verbot erlassen.", isCorrect:true},
+            {answer:"Zum Zwecke der Reinigung hat man die Arten unter Schutz gestellt.", isCorrect:false},
+            {answer:"Zum Schutz der Arten hat man das Gelände umweltfreundlich gestaltet.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Sie wurde vom Präsidenten zur Ministerin ernannt.",
+        answersList: [
+            {answer:"Kraft seines Amtes ernannte er sie zur Ministerin.", isCorrect:true},
+            {answer:"Sie wurde zur Ministerin ernannt, weil sie das kann.", isCorrect:false},
+            {answer:"Ohne dazu ernannt zu werden, hat sie das Amt übernommen.", isCorrect:false},
+        ]
+    },{
+        question:"Angesichts der konjunkturellen Lage wurden die Leitzinsen gesenkt.",
+        answersList: [
+            {answer:"Um die Wirtschaft anzukurbeln, wurden die Zinsen erhöht.", isCorrect:false},
+            {answer:"Weil die Wirtschaft floriert, werden die Kredite günstiger.", isCorrect:true},
+            {answer:"Obwohl es mehr Zinsgewinne gibt, wird die Wirtschaft nicht gefördert.", isCorrect:false},
+        ]
+    },{
+        question:"Wenn ich an Ihrer Stelle wäre, würde ich die Polizei informieren.",
+        answersList: [
+            {answer:"Ich würde die Polizei wohl eher nicht benachrichtigen.", isCorrect:false},
+            {answer:"Ich zögere noch, die Polizei persönlich anzurufen.", isCorrect:false},
+            {answer:"Wenn ich Sie wäre, würde ich die Polizei einschalten.", isCorrect:true},
+        ]
+    },{
+        question:"Wir haben das verlorene Gepäck nicht mehr zurückbekommen,",
+        answersList: [
+            {answer:"gleichwohl wir mehrmals reklamiert haben.", isCorrect:true},
+            {answer:"denn wir haben ständig reklamiert.", isCorrect:false},
+            {answer:"obwohl wir doch gar nicht reklamiert hatten.", isCorrect:false},
+        ]
+    },{
+        question:"Ich habe nicht mehr mit einer Rückerstattung der Kosten gerechnet.",
+        answersList: [
+            {answer:"Deshalb hat die Agentur auch nichts unternommen.", isCorrect:false},
+            {answer:"Gleichwohl die Reiseagentur keine Rückerstattung versprochen hat.", isCorrect:false},
+            {answer:"Nichtsdestotrotz hat die Agentur die Reisekosten zurückgezahlt.", isCorrect:true},
+        ]
+    },{
+        question:"Außerhalb der Öffnungszeiten",
+        answersList: [
+            {answer:"können Sie das Museum nur nach Voranmeldung besichtigen.", isCorrect:true},
+            {answer:"müssen Sie sich beim Sekretariat bewerben.", isCorrect:false},
+            {answer:"sollten Sie einen Besuch trotzdem in Betracht ziehen.", isCorrect:false},
+        ]
+    },{
+        question:"Das Essen im Hotelrestaurant ist ausgezeichnet.",
+        answersList: [
+            {answer:"Aber die Hotelbar ist noch schlechter.", isCorrect:false},
+            {answer:"Demgegenüber ist die Cafeteria nicht zu empfehlen.", isCorrect:true},
+            {answer:"Die Kneipe gegenüber bietet leider keine Mahlzeiten an.", isCorrect:false},
+        ]
+    },{
+        question:"Nach heutiger Erkenntnis war die Entwicklung von besonderer Bedeutung.",
+        answersList: [
+            {answer:"Heute wissen wir, dass es sich anders entwickelt hat.", isCorrect:false},
+            {answer:"Auch heute noch stellen viele die Verbesserungen in Frage.", isCorrect:false},
+            {answer:"Heutzutage weiß man, wie wichtig die Entwicklung war.", isCorrect:true},
+        ]
+    },{
+        question:"Nach umfangreichen Untersuchungen stellte sich das Werk als Fälschung heraus.",
+        answersList: [
+            {answer:"Schließlich haben die Forscher es als eine schlechte Kopie bezeichnet.", isCorrect:true},
+            {answer:"Anschließend haben die Spezialisten eine Kopie des Werkes angefertigt.", isCorrect:false},
+            {answer:"Nichtsdestotrotz konnte das Werk als Original bewertet werden.", isCorrect:false},
+        ]
+    },{
+        question:"Der Verdächtige ist in der Gesellschaft kaum aufgefallen.",
+        answersList: [
+            {answer:"Obwohl er einen ausschweifenden Lebensstil pflegte.", isCorrect:false},
+            {answer:"Obschon die wertvollen Gegenstände nirgendwo zu sehen waren.", isCorrect:false},
+            {answer:"Trotzdem haben die Zollfahnder einiges bei ihm gefunden.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
